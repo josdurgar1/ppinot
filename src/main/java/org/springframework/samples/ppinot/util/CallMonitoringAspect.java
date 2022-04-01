9ac1c6989ad84f3221f834bf36ca4e575a8fbec1
Jose Manuel <josdurgar1@alum.us.es>
2022-04-01 15:59:07 +0200

    Cambios

--- src/main/java/org/springframework/samples/ppinot/util/CallMonitoringAspect.java
+++ src/main/java/org/springframework/samples/ppinot/util/CallMonitoringAspect.java
@@ -35,7 +35,7 @@ import org.springframework.util.StopWatch;
  * @author Michael Isvy
  * @since 2.5
  */
-@ManagedResource("petclinic:type=CallMonitor")
+@ManagedResource("ppinot:type=CallMonitor")
 @Aspect
 public class CallMonitoringAspect {
 
--- src/main/java/org/springframework/samples/ppinot/util/EntityUtils.java
+++ src/main/java/org/springframework/samples/ppinot/util/EntityUtils.java
@@ -27,7 +27,7 @@ import org.springframework.samples.ppinot.model.BaseEntity;
  *
  * @author Juergen Hoeller
  * @author Sam Brannen
- * @see org.springframework.samples.petclinic.model.BaseEntity
+ * @see org.springframework.samples.ppinot.model.BaseEntity
  * @since 29.10.2003
  */
 public abstract class EntityUtils {
